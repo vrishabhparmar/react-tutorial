b12aeda9dea1edd321446317a8d21ab1dffd098e
vrishabhparmar <vrishabhluvcars@gmail.com>
2025-06-18 20:11:54 -0400

    Week 3

--- Week_2/my-app/src/components/Profile.jsx
+++ Week_2/my-app/src/components/Profile.jsx
@@ -8,16 +8,32 @@ const person = {
   }
 }
 
+const people = ['Creola Katherine Johnson: mathematician',
+'Mario José Molina-Pasquel Henríquez: chemist',
+'Mohammad Abdus Salam: physicist',
+'Percy Lavon Julian: chemist',
+'Subrahmanyan Chandrasekhar: astrophysicist']
+
+const peopleFunc = people.map(val => 
+  <div key={val}>
+    <img width={100} height={100} 
+    src={luffy} 
+        alt={person.name}
+      />
+      <h2>Name: {person.name}</h2>
+      <p>{val}</p>
+  </div>)
+
 function Profile(props){
   let person = props.person
     return (
       <div>
-        <img width={person.size} height={person.size} 
-        src={luffy} 
-            alt={person.name}
-          />
-          <h2>Name: {person.name}</h2>
+          {
+            peopleFunc
+          }
       </div>
+      
+      
     )
   }
 
